a635b17b32609bb1191ea7f9b213ab0fb53e2c71
Park-Tae_Woong <ptw0428@naver.com>
2022-07-10 01:26:22 +0900

    controller 연습

---
+++ src/main/java/com/sparta/springmvc/LoginController.java
@@ -0,0 +1,23 @@
+package com.sparta.springmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class LoginController {
+    @GetMapping("/login")
+    public String loginPage() {
+        return "redirect:/login-form.html";
+    }
+
+    @PostMapping("/login")
+    public String loginProcessParam(@RequestParam String id, @RequestParam String password, Model model) {
+        if (id.equals(password)){
+            model.addAttribute("loginId",id);
+        }
+        return "login-result";
+    }
+
+
+}
